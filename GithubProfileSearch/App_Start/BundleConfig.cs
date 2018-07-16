2456a58cbb849f34ab964e0e47ee2bf9b8ea6e3c
SgtZippy <sgt.zippy@gmail.com>
2018-07-15 20:03:21 -0600

    Initial changes for search project

---
+++ GithubProfileSearch/App_Start/BundleConfig.cs
@@ -0,0 +1,49 @@
+﻿using System.Web.Optimization;
+
+namespace GithubProfileSearch
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/GithubProfileSearch").Include(
+                "~/Apps/GithubProfileSearch/dist/GithubProfileSearch/runtime.js",
+                "~/Apps/GithubProfileSearch/dist/GithubProfileSearch/polyfills.js",
+                "~/Apps/GithubProfileSearch/dist/GithubProfileSearch/styles.js",
+                "~/Apps/GithubProfileSearch/dist/GithubProfileSearch/vendor.js",
+                "~/Apps/GithubProfileSearch/dist/GithubProfileSearch/main.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
+        }
+
+        /*
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        */
+    }
+}
